d5c1735e44d4612c2caeca2ff90c6ebd0bb0e76b
Jose M Delgado <jdelgado2002@gmail.com>
2024-06-21 14:45:59 -0500

    Url routing (#12)
    * Update localization strings for job search and skills match pages
    
    * chore: Update job filter form action and URL with serialized form data
    
    * chore: update form data from url
    
    * chore: Add new localization string for Jobs page, maintain url history with pagination
    
    * chore: Update job filter form action and URL with serialized form data
    
    * simplify code

--- aspnet-core/src/myvetjob.Web.Mvc/wwwroot/view-resources/Views/Jobs/Index.js
+++ aspnet-core/src/myvetjob.Web.Mvc/wwwroot/view-resources/Views/Jobs/Index.js
@@ -2,6 +2,9 @@ $(function () {
   "use strict";
   var page = 1; // Start from page 1
   var inProgress = false; // To prevent multiple simultaneous requests
+  const pageSize = 10;
+
+  updateFormWithSearchParams();
 
   $(window).scroll(function () {
     if (
@@ -15,42 +18,54 @@ $(function () {
 
   $("#jobFilterForm").on("submit", function (e) {
     e.preventDefault();
-    abp.ui.setBusy($(".job-openings"));
-    var form = $(this);
-    var url = form.attr("action");
-
     page = 1; // Reset the page variable
-
-    $.ajax({
-      type: "GET",
-      url: url,
-      data: form.serialize(), // This will include all the form fields in the AJAX request
-      success: function (data) {
-        // Insert the HTML returned by the server into the existing list of jobs
-        $(".job-openings").html(data);
-      },
-      complete: function () {
-        abp.ui.clearBusy($(".job-openings"));
-      },
-    });
+    loadMoreJobs();
   });
+
+  function updateFormWithSearchParams() {
+    var searchParams = new URLSearchParams(window.location.search);
+    searchParams.forEach(function (value, key) {
+      var formElement = $("#jobFilterForm [name='" + key + "']");
+      if (formElement) {
+        formElement.val(value);
+      }
+    });
+  }
+
+  function buildUrl(formSelector, page, pageSize) {
+    var formData = $(formSelector).serialize();
+    var dataObject = new URLSearchParams(formData);
+  
+    dataObject.append("skipCount", (page - 1) * pageSize);
+    dataObject.append("maxResultCount", pageSize);
+  
+    return $(formSelector).attr("action") + "?" + dataObject.toString();
+  }
 
   function loadMoreJobs() {
     inProgress = true;
-    abp.ui.setBusy($(".job-openings")); // Set the card body as busy during the request
+    // Clear existing jobs if this is the first page
+    if (page === 1) {
+      $(".job-openings").empty();
+    }
+    abp.ui.setBusy($(".job-openings")); 
 
-    var formData = $("#jobFilterForm").serialize();
-    var dataObject = new URLSearchParams(formData);
 
-    dataObject.append("skipCount", (page - 1) * 10);
-    dataObject.append("maxResultCount", 10); // Replace 10 with your actual page size
-
+    var newUrl = buildUrl("#jobFilterForm", page, pageSize); 
+  
     $.ajax({
-      url: "/Jobs/Index",
-      data: dataObject.toString(),
+      url: newUrl,
+      //data: dataObject.toString(),
       success: function (data) {
-        // Insert the HTML returned by the server into the existing list of jobs
-        $(".job-openings").append(data);
+        if (data.trim()) {
+          // Insert the HTML returned by the server into the existing list of jobs
+          $(".job-openings").append(data);
+          //update the URL with the new page number, only if new data was loaded
+          window.history.pushState({ path: newUrl }, '', newUrl);
+        } else {
+          console.log("No more jobs to load.");
+          page--; // Decrement page since no new data was loaded
+        }
         inProgress = false;
       },
       complete: function () {
